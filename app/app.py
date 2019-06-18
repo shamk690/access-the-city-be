141eb1953366433778bcd3a475f486b93fdad61d
Marco F. Iannuzzi <iannuzzimf@gmail.com>
2019-06-18 14:32:29 +0100

    connect to database

--- app/app.py
+++ app/app.py
@@ -1,12 +1,26 @@
-from flask import Flask, jsonify
+import os
+# import file from different folder
+from db.data.test_data.users import users
+from flask import Flask, request, jsonify
+from file import function
+
+from flask_sqlalchemy import SQLAlchemy
+
+
+# relative path
+dirname = os.path.relpath(__file__)
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://marco:password@localhost:5000/access_test'
+db = SQLAlchemy(app)
+
+print(db)
 
 
-@app.route('/')
-def helloWorld():
-    return jsonify({"msg": "ok"})
+@app.route('/api')
+def response():
+    return jsonify(users)
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
---
+++ app/db/data/test_data/users.py
@@ -0,0 +1,7 @@
+users = [{"email": "velit.justo.nec@risusDonecegestas.co.uk", "password": "WKS86ILH1XD"},
+         {"email": "eu.enim.Etiam@sapiengravidanon.net",
+          "password": "KKS21IHA7MA"},
+         {"email": "rutrum@interdumliberodui.edu", "password": "ESK61AUX1WN"},
+         {"email": "libero.Proin.sed@liberomauris.net",
+          "password": "ASN93QPX3SV"},
+         {"email": "venenatis.lacus.Etiam@nec.org", "password": "HHG56LCH0FA"}]
